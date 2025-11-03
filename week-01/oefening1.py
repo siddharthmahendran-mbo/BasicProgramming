6037c090297491eeb337e321a30336238f11f06b
Siddharth <siddharth.mahendranv@gmail.com>
2025-11-03 21:18:32 +0100

    Change: oefening1.py is gemaakt
    Type: Addition
    
    Why: Ik heb eerste stappen gezet door te programmeren in Python

--- week-01/oefening1.py
+++ week-01/oefening1.py
@@ -6,18 +6,27 @@ print("Welkom bij Python programmeren!")
 # In Python kun je informatie opslaan in variabelen.
 # Hieronder maken we een variabele met jouw naam en printen we deze.
 
-naam = "Student"
-print("Hallo", naam)
+naam = "Siddharth"
+print("Hallo", Siddharth)
 
 # === Oefening 1 ===
 # Maak een variabele 'leeftijd' met jouw leeftijd en print deze uit.
+leeftijd = 15
+print("Mijn leeftijd is", leeftijd)
 
 
 # === Oefening 2 ===
 # Maak drie variabelen: voornaam, achternaam en woonplaats en print deze netjes uit op 1 regel.
+voornaam = "Sid"
+achternaam = "Mahendran"
+woonplaats = "Madrid"
+
+print(voornaam, achternaam, woonplaats)
 
 
 # === Reflectie ===
 # Hoe voelt het om Python code te lezen en uit te voeren?
-
 # Wat vind je lastig?
+Het lezen en uitvoeren van Python code voelt leuk maar soms ook lastig. Het is fijn om te zien dat de computer doet wat ik heb geschreven.
+
+Wat ik moeilijk vind, is soms begrijpen wat de code precies doet. Ook is het lastig om fouten te vinden als iets niet werkt.
